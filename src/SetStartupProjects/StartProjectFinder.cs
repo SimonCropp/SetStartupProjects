b9092746baf94f7c75ddc2c1e1dbd67e6bc1ef1d
Simon Cropp <simon.cropp@gmail.com>
2016-05-25 20:58:41 +1000

    update to nunit 3

--- src/SetStartupProjects/StartProjectFinder.cs
+++ src/SetStartupProjects/StartProjectFinder.cs
@@ -7,40 +7,40 @@ namespace SetStartupProjects
 {
     /// <summary>
     ///  Helper class for extracting Visual Studio startup projects by looking at the projects contained in the solution directory and allplying some conventions.
-    /// 
+    ///
     /// The current conventions are as follows:
     ///   <list type="bullet">
-    ///     <item> 
-    ///       <description>'StartAction' is 'Program'.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>'OutputType' is 'Exe'.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>'OutputType' is 'WinExe'.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>Project extension is `ccproj` ie an Azure Cloud Service.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>Project type is 'ASP.NET MVC 1.0' ie '603C0E0B-DB56-11DC-BE95-000D561079B0'.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>Project type is 'ASP.NET MVC 2.0' ie 'F85E285D-A4E0-4152-9332-AB1D724D3325'.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>Project type is 'ASP.NET MVC 3.0' ie 'E53F8FEA-EAE0-44A6-8774-FFD645390401'.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>Project type is 'ASP.NET MVC 4.0' ie 'E3E379DF-F4C6-4180-9B81-6769533ABE47'.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>Project type is 'Web Application' ie '349C5851-65DF-11DA-9384-00065B846F21'.</description> 
-    ///     </item> 
-    ///     <item> 
-    ///       <description>Project type is 'Web Site' ie 'E24C65DC-7377-472B-9ABA-BC803B73C61A'.</description> 
-    ///     </item> 
-    ///   </list> 
+    ///     <item>
+    ///       <description>'StartAction' is 'Program'.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>'OutputType' is 'Exe'.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>'OutputType' is 'WinExe'.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>Project extension is `ccproj` ie an Azure Cloud Service.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>Project type is 'ASP.NET MVC 1.0' ie '603C0E0B-DB56-11DC-BE95-000D561079B0'.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>Project type is 'ASP.NET MVC 2.0' ie 'F85E285D-A4E0-4152-9332-AB1D724D3325'.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>Project type is 'ASP.NET MVC 3.0' ie 'E53F8FEA-EAE0-44A6-8774-FFD645390401'.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>Project type is 'ASP.NET MVC 4.0' ie 'E3E379DF-F4C6-4180-9B81-6769533ABE47'.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>Project type is 'Web Application' ie '349C5851-65DF-11DA-9384-00065B846F21'.</description>
+    ///     </item>
+    ///     <item>
+    ///       <description>Project type is 'Web Site' ie 'E24C65DC-7377-472B-9ABA-BC803B73C61A'.</description>
+    ///     </item>
+    ///   </list>
     /// </summary>
     public class StartProjectFinder
     {
@@ -53,7 +53,7 @@ namespace SetStartupProjects
             Guard.AgainstNonExistingFile(solutionFile, "solutionFile");
 
             return from project in SolutionProjectExtractor.GetAllProjectFiles(solutionFile)
-                   where ShouldIncludeProjectFile(project) 
+                   where ShouldIncludeProjectFile(project)
                    select project.Guid;
         }
 
@@ -110,7 +110,7 @@ namespace SetStartupProjects
             var outputType = propertyGroup.Element("OutputType").Value;
             if (
                 outputType == "Exe" ||
-                outputType == "WinExe" 
+                outputType == "WinExe"
                 )
             {
                 return true;
@@ -132,12 +132,12 @@ namespace SetStartupProjects
 
         protected internal List<string> DefaultIncludedGuids = new List<string>
         {
-            "603C0E0B-DB56-11DC-BE95-000D561079B0", //ASP.NET MVC 1.0	
-            "F85E285D-A4E0-4152-9332-AB1D724D3325", //ASP.NET MVC 2.0	
-            "E53F8FEA-EAE0-44A6-8774-FFD645390401", //ASP.NET MVC 3.0	
-            "E3E379DF-F4C6-4180-9B81-6769533ABE47", //ASP.NET MVC 4.0	
-            "349C5851-65DF-11DA-9384-00065B846F21", //Web Application	
-            "E24C65DC-7377-472B-9ABA-BC803B73C61A", //Web Site	
+            "603C0E0B-DB56-11DC-BE95-000D561079B0", //ASP.NET MVC 1.0
+            "F85E285D-A4E0-4152-9332-AB1D724D3325", //ASP.NET MVC 2.0
+            "E53F8FEA-EAE0-44A6-8774-FFD645390401", //ASP.NET MVC 3.0
+            "E3E379DF-F4C6-4180-9B81-6769533ABE47", //ASP.NET MVC 4.0
+            "349C5851-65DF-11DA-9384-00065B846F21", //Web Application
+            "E24C65DC-7377-472B-9ABA-BC803B73C61A", //Web Site
         };
     }
 }
--- src/Tests/SolutionProjectExtractorTests.cs
+++ src/Tests/SolutionProjectExtractorTests.cs
@@ -10,7 +10,7 @@ public class SolutionProjectExtractorTests
     [Test]
     public void GetAllProjectFiles()
     {
-        var samplesolutionTxt = Path.GetFullPath("SampleSolution.txt");
+        var samplesolutionTxt = Path.Combine(TestContext.CurrentContext.TestDirectory, "SampleSolution.txt");
         var directoryName = Path.GetDirectoryName(samplesolutionTxt);
         var allProjectFiles = SolutionProjectExtractor.GetAllProjectFiles(samplesolutionTxt);
         ObjectApprover.VerifyWithJson(allProjectFiles, s => s.Replace(@"\\", @"\").Replace(directoryName, ""));
