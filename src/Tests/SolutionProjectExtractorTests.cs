7870eb860246f45ef32646cffc2d4f31a1bdf4c7
Simon Cropp <simon.cropp@gmail.com>
2019-07-31 18:53:28 +1000

    VerifyWithJson => Verify

--- src/Tests/SolutionProjectExtractorTests.cs
+++ src/Tests/SolutionProjectExtractorTests.cs
@@ -11,7 +11,7 @@ public class SolutionProjectExtractorTests :
     public void GetAllProjectFiles()
     {
         var allProjectFiles = SolutionProjectExtractor.GetAllProjectFiles("SampleSolution.txt");
-        ObjectApprover.VerifyWithJson(allProjectFiles, s => s.Replace(@"\\", @"\").Replace(Directory.GetCurrentDirectory(), ""));
+        ObjectApprover.Verify(allProjectFiles, s => s.Replace(@"\\", @"\").Replace(Directory.GetCurrentDirectory(), ""));
     }
 
     public SolutionProjectExtractorTests(ITestOutputHelper output) :
