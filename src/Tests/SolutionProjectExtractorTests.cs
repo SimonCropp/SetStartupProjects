46e0d1c51bc2e7422ea750a73f90fcf83c46307b
Simon Cropp <simon.cropp@gmail.com>
2019-09-07 14:57:32 +1000

    Xunit.ApprovalTests

--- src/Tests/SolutionProjectExtractorTests.cs
+++ src/Tests/SolutionProjectExtractorTests.cs
@@ -4,7 +4,7 @@ using Xunit;
 using Xunit.Abstractions;
 
 public class SolutionProjectExtractorTests :
-    XunitLoggingBase
+    XunitApprovalBase
 {
     [Fact]
     public void GetAllProjectFiles()
--- src/Tests/StartProjectFinderTests.cs
+++ src/Tests/StartProjectFinderTests.cs
@@ -6,7 +6,7 @@ using Xunit;
 using Xunit.Abstractions;
 
 public class StartProjectFinderTests :
-    XunitLoggingBase
+    XunitApprovalBase
 {
     [Fact]
     public void Exe_from_OutputType()
