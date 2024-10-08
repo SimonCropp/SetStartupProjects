0154f0d64a5bfe74793361bb4e95c9817d2cedb9
David Boike <david.boike@gmail.com>
2024-10-08 14:13:30 -0500

    More cross-platform fixes

--- src/SetStartupProjects/SolutionProjectExtractor.cs
+++ src/SetStartupProjects/SolutionProjectExtractor.cs
@@ -18,7 +18,8 @@ public static class SolutionProjectExtractor
             var strings = line.Split(["\", \""], StringSplitOptions.RemoveEmptyEntries);
             var guidType = strings[0].Split('{', '}')[1];
             var guid = strings[2].Trim('{', '}', '"');
-            var fullPath = Path.GetFullPath(Path.Combine(solutionDirectory, strings[1]));
+            var relativePath = Path.DirectorySeparatorChar == '\\' ? strings[1] : strings[1].Replace('\\', Path.DirectorySeparatorChar);
+            var fullPath = Path.GetFullPath(Path.Combine(solutionDirectory, relativePath));
             if (guidType == "2150E333-8FDC-42A3-9474-1A3956D46DE8")
             {
                 //this is a Solution Folder and can be ignored
@@ -26,7 +27,7 @@ public static class SolutionProjectExtractor
             }
             yield return new(
                 fullPath: fullPath,
-                relativePath: strings[1],
+                relativePath: relativePath,
                 guid: guid
             );
         }
