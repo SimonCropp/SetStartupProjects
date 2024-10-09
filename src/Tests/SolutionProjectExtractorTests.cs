9860ab3807dbb66ac02d109e77062b011ffda638
David Boike <david.boike@gmail.com>
2024-10-09 09:01:51 -0500

    Make test OS specific, and fix additional issue with manually-specified start projects

--- src/Tests/SolutionProjectExtractorTests.cs
+++ src/Tests/SolutionProjectExtractorTests.cs
@@ -4,13 +4,17 @@
     public Task GetAllProjectFiles()
     {
         var files = SolutionProjectExtractor.GetAllProjectFiles("SampleSolution.txt");
-        return Verify(
-            files.Select(_ =>
-                new
-                {
-                    _.Guid,
-                    _.RelativePath,
-                    FullPath = new FileInfo(_.FullPath),
-                }));
+        var toVerify = files.Select(_ =>
+            new
+            {
+                _.Guid,
+                _.RelativePath,
+                FullPath = new FileInfo(_.FullPath),
+            });
+
+        var settings = new VerifySettings();
+        settings.UniqueForOSPlatform();
+
+        return Verify(toVerify, settings);
     }
 }
